73beeb93b92c91a8741dabc514cd2755ec9b4207
laloinsane <calisto.michel@gmail.com>
2021-09-05 02:26:34 -0300

    implementation of smooth movement

--- scripts/Level-01/Player.gd
+++ scripts/Level-01/Player.gd
@@ -1,15 +1,20 @@
 extends KinematicBody2D
 
+const acceleration = 10
+const max_speed = 100
+const friction = 10
 var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-
+	input_vector = input_vector.normalized()
+	
 	if input_vector != Vector2.ZERO:
-		velocity = input_vector
+		velocity += input_vector * acceleration * delta
+		velocity = velocity.clamped(max_speed * delta)
 	else:
-		velocity = Vector2.ZERO
-
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+		
 	move_and_collide(velocity)
