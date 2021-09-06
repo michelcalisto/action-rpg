2d0ec1b93f5736f73c9c2fd7d2216d6ded27ac2c
laloinsane <calisto.michel@gmail.com>
2021-09-06 18:35:16 -0300

    implementations of basics player animations

--- scripts/Level-01/Player.gd
+++ scripts/Level-01/Player.gd
@@ -5,6 +5,8 @@ const max_speed = 80
 const friction = 500
 var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -13,8 +15,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 		
 	velocity = move_and_slide(velocity)
