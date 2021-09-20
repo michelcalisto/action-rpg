dee963bf394239a66aabbb138ea1b3a94d4a260b
laloinsane <calisto.michel@gmail.com>
2021-09-20 12:50:50 -0300

    implementations of roll

--- scripts/Level-01/Player.gd
+++ scripts/Level-01/Player.gd
@@ -3,7 +3,9 @@ extends KinematicBody2D
 const acceleration = 500
 const max_speed = 80
 const friction = 500
+const roll_speed = 120
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.LEFT
 enum {move, roll, attack}
 var state = move
 onready var animationPlayer = $AnimationPlayer
@@ -29,17 +31,22 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 		
-	velocity = move_and_slide(velocity)
+	move()
 	
+	if Input.is_action_just_pressed("roll"):
+		state = roll
+		
 	if Input.is_action_just_pressed("attack"):
 		state = attack
 
@@ -51,4 +58,13 @@ func attack_animation_finished():
 	state = move
 	
 func roll_state(delta):
-	pass
+	velocity = roll_vector * roll_speed
+	animationState.travel("Roll")
+	move()
+	
+func roll_animation_finished():
+	velocity = velocity * 0.8
+	state = move
+
+func move():
+	velocity = move_and_slide(velocity)
